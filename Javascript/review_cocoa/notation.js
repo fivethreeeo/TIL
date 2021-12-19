41ff09507e212a6fdc757398f9553f523fe947d7
Sangbeom Heo <mansaout@gmail.com>
2021-12-20 00:46:15 +0900

    코코아 다시 풀기 notation 노트 작성

--- Javascript/review_cocoa/notation.js
+++ Javascript/review_cocoa/notation.js
@@ -4,8 +4,9 @@
   [프로그래밍 순서]
     1) 예외처리 -> 에러 메시지 & 종료
     2) 게임에서 말해야 할 총 숫자를 배열로 구함. totalNumbers
-    3) totalNumbers의 숫자들을 n진수로 변환 -> n진수로 변환된 숫자들을 전부 1글자로 split. 
-    4) 전체 답변에서 filter로 내 순서만 찾아 배열로 구함 -> 내 차례 답변
+    3) totalNumbers의 숫자들을 n진수로 변환. convertedNumbers
+    4) convertedNumbers를 한 글자씩 분리. allAnswers
+    4) allAnswers에서 filter로 내 순서만 찾아 배열로 구함. myAnswer
 
   [예외처리]
     1) 변경할 진수가 2~16이 아닐 경우
@@ -13,43 +14,43 @@
 
   [노트]
     1) 구조 변경
-      - 이전 풀이는 사용자 입력이 circle일 경우, getCircle를 호출하고, getCircle 안에서 매개변수에 따라 getCircles를 또 호출하는 구조이다.
-      - 이럴 경우, 메인 함수 getArea만 보고는 getCircles까지 가는 로직을 정확하게 이해하기 어렵다.
-      - 중간에 사용자 입력에 맞춰 정확한 figure를 구하는 과정을 추가했다. 그리고 그 figure에 맞춰 필요한 함수를 바로 출력할 수 있도록 수정했다.
-      - 메인 함수 getArea만 봐도 전체 구조를 더 잘 파악할 수 있도록 변경했다.
-
-    2) 예외처리 추가
-      - 기존보다 더 넓은 범위의 예외처리를 추가했다.
-
-    3) switch문 -> if문 변경
-      - 개인적으로 if문이 더 간결하고 가독성이 뛰어난 것 같아서 변경했다.
-      - 문 안에서 특별한 로직이 있는게 아니라 한 줄짜리 간단한 식이라 {}, break, case 등을 더 써야하는 switch문이 더 복잡한 것 같다.
-      - if문으로 한 줄로 간단하게 처리가 가능하다.
+      - 스프레드 연산자로 기존 메소드, 함수 로직을 대체. split...
+      - 기존 for문을 사용한 로직을 스프레드 연산자로 대체.
+      - 가독성을 더 좋게 바꿈
 
     4)
-      - Math.PI
-      - Math.pow(숫자, 제곱)
+      - parseInt(30, 8) // 8진수의 30을 10진수 숫자로 변환
+      - (30).toString(2) // 10진수 숫자 30을 2진수 숫자로 변환
+      - Array.prototype.keys()
+        - 배열의 각 인덱스를 키 값으로 가지는 새로운 Array Iterator를 반환
+        - 빈 인덱스를 무시하지 않음.
+        - Iterator기 때문에 스프레드 연산자 사용 가능
 */
 
-function playNotationGame(notation, numberCount, playerCount, myTurn) {
+const playNotationGame = (notation, numberCount, playerCount, myTurn) => {
   if (!isVaildArgs(notation, playerCount, myTurn)) return;
 
   const totalCount = numberCount * playerCount;
   const totalNumbers = [...Array(totalCount).keys()];
-  const convertedNumbers = totalNumbers.map((num) => num.toString(notation));
+  const convertedNumbers = convertNumber(totalNumbers, notation);
 
-  console.log(convertedNumbers);
-  const allAnswer = [];
+  const allAnswers = [...convertedNumbers.join('')];
+  const myAnswer = getPlayerAnswer(allAnswers, playerCount, myTurn);
 
-  const myFirstIndex = myTurn - 1;
-  const myAnswer = allAnswer.filter(
-    (n, index) => index % playerCount === myFirstIndex
-  );
+  printResult(allAnswers, myAnswer);
+};
 
-  console.log(`\n전체 답변: ${allAnswer}` + `\n내 차례 답변: ${myAnswer}`);
-}
+const convertNumber = (numbers, notation) => numbers.map((number) => number.toString(notation));
 
-function isVaildArgs(notation, playerCount, myTurn) {
+const getPlayerAnswer = (allAnswers, playerCount, playerTurn) => {
+  const playerFirstIndex = playerTurn - 1;
+  return allAnswers.filter((n, index) => index % playerCount === playerFirstIndex);
+};
+
+const printResult = (allAnswers, myAnswer) =>
+  console.log(`\nallAnswers: ${allAnswers}` + `\nmyAnswer: ${myAnswer}`);
+
+const isVaildArgs = (notation, playerCount, myTurn) => {
   if (notation < 1 || notation > 17) {
     console.log('진수는 2~16 중에 입력하세요.');
     return false;
@@ -59,7 +60,7 @@ function isVaildArgs(notation, playerCount, myTurn) {
     return false;
   }
   return true;
-}
+};
 
 playNotationGame(2, 3, 3, 2);
 
